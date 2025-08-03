e4efec057ffe78f16e7ce183ca54b885ff6879a7
shuraih-ali <shuraihali003@gmail.com>
2025-08-03 09:39:50 +0500

    apk

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,33 @@
+import 'package:flutter/foundation.dart'; // For kDebugMode
+import 'package:flutter/material.dart';
+import 'package:device_preview/device_preview.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:to_do/pages/home_page.dart';
-import 'package:flutter/material.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Hive.initFlutter();
-  var box = await Hive.openBox('mybox');
+  await Hive.openBox('mybox');
 
-  runApp(const MyApp());
+  runApp(
+    DevicePreview(
+      enabled: kDebugMode, 
+      builder: (context) => const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      builder: DevicePreview.appBuilder,
+      locale: DevicePreview.locale(context),
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
       theme: ThemeData.dark(),
+      home: const HomePage(),
     );
   }
 }
